a59facc84becd138e9c54c1114925c9c4162cf78
cuongtk1002 <tkcuong1002@gmail.com>
2022-10-26 11:46:13 +0700

    array

---
+++ array-two.js
@@ -0,0 +1,36 @@
+// Bài 1:
+// let a = [
+//     [1, 2, 1, 24],
+//     [8, 11, 9, 4],
+//     [7, 0, 7, 27],
+//     [7, 4, 28, 14],
+//     [3, 10, 26, 7]
+// ];
+// console.log(a);
+//
+// Bài 2:
+// let newArray = ['c', 's', 'c', '2', '6', '1'];
+//     newArray.reverse();
+// console.log(newArray);
+//
+// Bài 3:
+// let array = ['c', 'o', 'd', 'e', 'g', 'y', 'm']
+// array.length = 7;
+// console.log(array);
+//
+// Bài 4:
+// var str = "Hello Cuong Code gym";
+// var array = str.split(" ");
+// console.log(array);
+// array.length;
+// console.log(array.length);
+
+Bài 5:
+var name = "Tran Khac Cuong ";
+var str = "hello code gym";
+var full = name.concat(str);
+console.log(full);
+
+
+
+
